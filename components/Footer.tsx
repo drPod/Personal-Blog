9455616c2965cd487dcab510ab8c6a9cc0048d38
notMrpod <darsh.pod@gmail.com>
2023-08-30 11:56:54 -0500

    Footer Logo Removal

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import CosmicLogo from './logos/CosmicLogo';
+// import CosmicLogo from './logos/CosmicLogo';
 
 export default function Footer(): JSX.Element {
   return (
     <footer className='mx-auto mt-8 flex w-full max-w-3xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base'>
       <a href='https://github.com/drPod' target='_blank' className='no-underline'>
         <div className='flex items-center space-x-2'>
-          <CosmicLogo />
           <span className='text-zinc-700 dark:text-zinc-300'>Website developed and maintained by Darsh Poddar</span>
         </div>
       </a>
